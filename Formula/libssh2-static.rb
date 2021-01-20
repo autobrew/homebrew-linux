845ffb615b38517f0d5ab9a3f58d29fe73ec0966
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-20 15:41:52 +0000

    Add static formulae for libgit2

---
+++ Formula/libssh2-static.rb
@@ -0,0 +1,58 @@
+class Libssh2Static < Formula
+  desc "C library implementing the SSH2 protocol"
+  homepage "https://libssh2.org/"
+  url "https://libssh2.org/download/libssh2-1.9.0.tar.gz"
+  sha256 "d5fb8bd563305fd1074dda90bd053fb2d29fc4bce048d182f96eaa466dfadafd"
+  license "BSD-3-Clause"
+  revision 1
+
+  livecheck do
+    url "https://libssh2.org/download/"
+    regex(/href=.*?libssh2[._-]v?(\d+(?:\.\d+)+)\./i)
+  end
+
+  head do
+    url "https://github.com/libssh2/libssh2.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "openssl@1.1"
+
+  uses_from_macos "zlib"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --disable-debug
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --disable-examples-build
+      --with-openssl
+      --with-libz
+      --with-libssl-prefix=#{Formula["openssl@1.1"].opt_prefix}
+      --with-pic
+    ]
+
+    system "./buildconf" if build.head?
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <libssh2.h>
+
+      int main(void)
+      {
+      libssh2_exit();
+      return 0;
+      }
+    EOS
+
+    system ENV.cc, "test.c", "-L#{lib}", "-lssh2", "-o", "test"
+    system "./test"
+  end
+end
---
+++ Formula/pcre-static.rb
@@ -0,0 +1,53 @@
+class PcreStatic < Formula
+  desc "Perl compatible regular expressions library"
+  homepage "https://www.pcre.org/"
+  url "https://ftp.pcre.org/pub/pcre/pcre-8.44.tar.bz2"
+  mirror "https://www.mirrorservice.org/sites/ftp.exim.org/pub/pcre/pcre-8.44.tar.bz2"
+  sha256 "19108658b23b3ec5058edc9f66ac545ea19f9537234be1ec62b714c84399366d"
+  license "BSD-3-Clause"
+
+  livecheck do
+    url "https://ftp.pcre.org/pub/pcre/"
+    regex(/href=.*?pcre[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  head do
+    url "svn://vcs.exim.org/pcre/code/trunk"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  uses_from_macos "bzip2"
+  uses_from_macos "zlib"
+
+  def install
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --enable-utf8
+      --enable-pcre8
+      --enable-pcre16
+      --enable-pcre32
+      --enable-unicode-properties
+      --enable-pcregrep-libz
+      --enable-pcregrep-libbz2
+      --with-pic
+    ]
+
+    # JIT not currently supported for Apple Silicon
+    args << "--enable-jit" unless Hardware::CPU.arm?
+
+    system "./autogen.sh" if build.head?
+    system "./configure", *args
+    system "make"
+    ENV.deparallelize
+    system "make", "test"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/pcregrep", "regular expression", "#{prefix}/README"
+  end
+end
